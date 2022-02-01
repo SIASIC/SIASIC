3199ec1c7c738fb10ce49b558fe43f71437f5984
irhasalif <irhasalif@gmail.com>
2022-02-01 18:39:23 +0700

    Initial django JWT

--- api/views.py
+++ api/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
+
+@api_view(['GET'])
 def get_routes(request):
-    return JsonResponse('Halo', safe=False)
+    return Response("Halo")
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
