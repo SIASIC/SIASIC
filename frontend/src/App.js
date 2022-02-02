b475ec0e1dcad91b27e1bb9059000d04d40194c9
irhasalif <irhasalif@gmail.com>
2022-02-02 13:12:25 +0700

    Auth Provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,39 @@
-import React, { lazy } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer'
-import PrivateRoute from './utils/PrivateRoute'
+import React, { lazy } from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
+import AccessibleNavigationAnnouncer from "./components/AccessibleNavigationAnnouncer";
+import PrivateRoute from "./utils/PrivateRoute";
+import AuthContext, { AuthProvider } from "./context/AuthContext";
 
-const Layout = lazy(() => import('./containers/Layout'))
-const Login = lazy(() => import('./pages/Login'))
-const CreateAccount = lazy(() => import('./pages/CreateAccount'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+const Layout = lazy(() => import("./containers/Layout"));
+const Login = lazy(() => import("./pages/Login"));
+const CreateAccount = lazy(() => import("./pages/CreateAccount"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
 
 function App() {
-  return (
-    <>
-      <Router>
-        <AccessibleNavigationAnnouncer />
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/create-account" component={CreateAccount} />
-          <Route path="/forgot-password" component={ForgotPassword} />
+	return (
+		<>
+			<Router>
+				<AuthProvider>
+					<AccessibleNavigationAnnouncer />
+					<Switch>
+						<Route path="/create-account" component={CreateAccount} />
+						<Route path="/forgot-password" component={ForgotPassword} />
+						<Route path="/login" component={Login} />
 
-          {/* Place new routes over this */}
-          <PrivateRoute path="/app" component={Layout} />
-          {/* If you have an index page, you can remothis Redirect */}
-          <Redirect exact from="/" to="/login" />
-        </Switch>
-      </Router>
-    </>
-  )
+						{/* Place new routes over this */}
+						<PrivateRoute path="/app" component={Layout} />
+						{/* If you have an index page, you can remothis Redirect */}
+						<Redirect exact from="/" to="/login" />
+					</Switch>
+				</AuthProvider>
+			</Router>
+		</>
+	);
 }
 
-export default App
+export default App;
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 
 import ImageLight from '../assets/img/login-office.jpeg'
 import ImageDark from '../assets/img/login-office-dark.jpeg'
-import { GithubIcon, TwitterIcon } from '../icons'
+// import { GithubIcon, TwitterIcon } from '../icons'
 import { Label, Input, Button } from '@windmill/react-ui'
+import AuthContext from '../context/AuthContext'
 
 function Login() {
+  // let {name} = useContext(AuthContext)
   return (
     <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
