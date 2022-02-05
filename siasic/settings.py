fcb12462cf5d35535bd1517fdc146de26006d013
irhasalif <irhasalif@gmail.com>
2022-02-05 10:40:16 +0700

    postgresql integration & update requirment

--- siasic/settings.py
+++ siasic/settings.py
@@ -123,10 +123,21 @@ WSGI_APPLICATION = 'siasic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'siasic',
+        'USER': 'siasic',
+        'PASSWORD': 'siasic',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
